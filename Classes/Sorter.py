18c138b10ed955bbeba8b3083a2e34d2b8dacff7
SydneyDF <maximumsupernova@gmail.com>
2021-10-11 15:22:39 +0800

    Sorter Created and Tested

---
+++ Classes/Sorter.py
@@ -0,0 +1,38 @@
+import time
+
+from sampleInput import sampleInput as _sampleList
+
+
+class Sorter:
+    # WE are going to cheat and use a simpler sorting algorithm to implement, Bubble sort.
+    # quicksort will take longer to implement.
+    def __init__(self):
+        # Class initialized by importing unsorted list from /sampleInput.py
+        self.sampleList = _sampleList
+
+    def bubble_sort(self):
+        # Record amount
+        swap_count = 0
+        # With this loop we start at the full length of list end goal of reaching 0, each iteration decrease by 1
+        # equates to range(133 as the start, 0 to be the end and each loop take 1 away till end
+        for x in range(len(self.sampleList) - 1, 0, - 1):
+            # get current x and pass into 2nd layer for
+            for y in range(x):
+                # compare current to the next index of sampleList
+
+                # Swapping occurs via variable packing
+                # self.sampleList[y], self.sampleList[y + 1] = self.sampleList[y + 1], self.sampleList[y]
+                if self.sampleList[y] > self.sampleList[y + 1]:
+                    self.sampleList[y], self.sampleList[y + 1] = self.sampleList[y + 1], self.sampleList[y]
+                    swap_count += 1
+                    print(f"Swapping Operations count: {swap_count}")
+
+        # Silly Print
+        print("As you can see even though bubble sort is the quickest to code...")
+        for x in range(3):
+            time.sleep(1)
+            print("." * x)
+            if x == 2:
+                print("It is probably the most inefficient")
+
+        return self.sampleList
---
+++ Tests/Test_Sorting.py
@@ -0,0 +1,18 @@
+import unittest
+
+from Classes.Sorter import Sorter
+from sampleInput import sampleInput
+
+
+# To Test our Sorter Class
+class Test_InitialSort(unittest.TestCase):
+    def setUp(self):
+        self.TestObj = Sorter()
+
+    # Compare to built in python function that returns sorted list from parsed in parameter
+    def test_sorter(self):
+        self.assertListEqual(self.TestObj.bubble_sort(), sorted(sampleInput))
+
+
+if __name__ == '__main__':
+    unittest.main()
